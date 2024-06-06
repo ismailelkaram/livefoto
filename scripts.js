9db426c6470cb569f6cad57337597a90e0040f00
ismailelkaram <72134967+ismailelkaram@users.noreply.github.com>
2024-06-06 05:26:54 +0500

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,3 @@
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
-
-function scrollToBottom() {
-  window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
-}
